70bfaedba00bc27cee55111ff46df7d5fa2084ed
sid120 <siddharthrakshe12@gmail.com>
2023-04-20 01:05:09 +0530

    created Project page ,Contact page

--- blog/src/Components/Projects.js
+++ blog/src/Components/Projects.js
@@ -1,3 +1,104 @@
-export const Project = () => {
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import { ProjectCard } from "./ProjectCards";
 
-}
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <Tab.Container id="projects-tab" defaultActiveKey="first">
+            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">  
+            <Nav.Item>
+                <Nav.Link eventKey="first">Tab One </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Tab Two </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">
+                  Tab Three 
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+                <Tab.Pane eventKey="first">
+                <Row>
+                    {
+                        projects.map((project,index)=>{
+                            return(
+                                <ProjectCard 
+                                    key={index}
+                                    {...project}
+                                />
+                            )
+                        })
+                    }
+                </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second"><Row>
+                {
+                    projects.map((project,index)=>{
+                        return(
+                            <ProjectCard 
+                                key={index}
+                                {...project}
+                            />
+                        )
+                    })
+                }
+            </Row></Tab.Pane>
+                <Tab.Pane eventKey="third">Lorem</Tab.Pane>
+            </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
